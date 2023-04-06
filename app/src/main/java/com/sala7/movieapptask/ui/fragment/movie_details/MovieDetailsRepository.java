996920325b032ca2a3244128faa708cca762fdbc
aSalahh <aaahmedsalah65@gmail.com>
2023-04-07 00:09:19 +0200

    init hilt and retrofit service

---
+++ app/src/main/java/com/sala7/movieapptask/ui/fragment/movie_details/MovieDetailsRepository.java
@@ -0,0 +1,42 @@
+package com.sala7.movieapptask.ui.fragment.movie_details;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.sala7.movieapptask.data.remote.MoviesApiService;
+import com.sala7.movieapptask.pojo.response.MovieDetailsResponse;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MovieDetailsRepository {
+    private final MoviesApiService moviesApiService;
+    private final MutableLiveData<MovieDetailsResponse> tvShowResponseMutableLiveData = new MutableLiveData<>();
+
+
+    @Inject
+    public MovieDetailsRepository(MoviesApiService moviesApiService) {
+        this.moviesApiService = moviesApiService;
+    }
+
+    public LiveData<MovieDetailsResponse> getMovieDetails(int movieId) {
+        moviesApiService.getMovieDetails(movieId).enqueue(new Callback<MovieDetailsResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<MovieDetailsResponse> call, @NonNull Response<MovieDetailsResponse> response) {
+                tvShowResponseMutableLiveData.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieDetailsResponse> call, @NonNull Throwable t) {
+                tvShowResponseMutableLiveData.setValue(null);
+            }
+        });
+        return tvShowResponseMutableLiveData;
+    }
+
+
+}
