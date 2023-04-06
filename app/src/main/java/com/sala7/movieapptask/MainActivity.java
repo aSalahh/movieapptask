c868af9617ff94f6d94db81f68e240cee8126026
aSalahh <aaahmedsalah65@gmail.com>
2023-04-06 22:51:01 +0200

    init project

---
+++ app/src/main/java/com/sala7/movieapptask/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sala7.movieapptask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
